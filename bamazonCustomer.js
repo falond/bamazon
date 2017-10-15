5318b55de37abc58c4a4d64aa0cfb98ce87e440e
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-14 21:15:13 -0400

    sql connect

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,55 @@
+//NPM mysql
 var mysql = require("mysql");
+//NPM inquirer
 var inquirer = require("inquirer");
+//Connection to mysql
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
+
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
+
+        case "Search for a specific song":
+          songSearch();
+          break;
+      }
+    });
+}
