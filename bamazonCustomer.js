ca3df25af87bed8504d9f93f1ad927f53c25f433
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-15 15:03:18 -0400

    error fix

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,10 @@ var inquirer = require("inquirer");
 //Takes all command lines arguments
 var inputString = process.argv;
 var command = inputString[2];
+
+
+
+// *************************************************SQL CONNECTION**********************************************
 //Connection to mysql
 var connection = mysql.createConnection({
   host: "localhost",
@@ -20,45 +24,62 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  console.log("\nWELCOME TO BAMAZON!\n");
+  displayProducts();
 });
 
+// *************************************************DISPLAY PRODUCTS FUNCTION**********************************************
+ function displayProducts() {
+  connection.query("SELECT item_id, product_name, department_name, price, stock_quanity FROM products", function(err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+       // console.log("Item ID: " + res[i].item_id + " | " + 
+       //  "Product Name: " + res[i].product_name + " | " + 
+       //  "Department Name: " + res[i].department_name + " | " + 
+       //  "Price: $" + res[i].price);
+           console.log("------------------------------------------------");
+           console.log("Item ID: " + res[i].item_id);
+           console.log("Product Name: " + res[i].product_name);
+           console.log("Department Name: " + res[i].department_name);
+           console.log("Price: $" + res[i].price);
+     }
+
+    connection.end();
+    askQuestions();
+  });
+}
+// *************************************************ASK QUESTIONS FUNCTION**********************************************
 function askQuestions() {
   inquirer
-    .prompt({
+    .prompt([
+    { 
       name: "products",
-      type: "list",
-      message: "What would you like to buy?",
-      choices: [
-        "GOPRO HERO5",
-        "Diamondback Bicycle",
-        "Basic Tent",
-        "Spalding NBA Street Basketball", 
-        "Crosley Record Player",
-        "TCL 32-Inch TV",
-        "Echo Dot",
-        "FIFA 18 PS3",
-        "Kayak",
-        "Snowboard"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-        case "Find songs by artist":
-          artistSearch();
-          break;
-
-        case "Find all artists who appear more than once":
-          multiSearch();
-          break;
-
-        case "Find data within a specific range":
-          rangeSearch();
-          break;
-
-        case "Search for a specific song":
-          songSearch();
-          break;
-      }
+      type: "input",
+      message: "Please enter the ID number of the item you would like to buy?",
+    },
+    {
+      name: "units",
+      type: "input",
+      message: "Please eneter how many units would you like to buy?"
+    }  
+    ]).then(function(answer) {
+      connection.query(
+        "UPDATE INTO products SET ? WHERE ?",
+        [
+              {
+                stock_quanity: answer.units
+              },
+              {
+                item_id: answer.products
+              }
+        ],
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          displayProducts();
+        }
+      );
     });
 }
